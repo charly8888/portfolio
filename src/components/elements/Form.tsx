4c69ccb6c4aaece81bc89a8862df11076b6746bd
German Hornus <hornusgerman@gmail.com>
2022-04-29 00:13:07 -0300

    ading netlify name3

--- src/components/elements/Form.tsx
+++ src/components/elements/Form.tsx
@@ -5,7 +5,7 @@ import css from './form.module.scss'
 
 export const Form = () => {
   return (
-    <form className={css.formContainer}  data-netlify="true">
+    <form className={css.formContainer}  data-netlify="true" name="contact" method="POST">
       <label>
         <p>Name</p>
         <input type="text" name="name" />
