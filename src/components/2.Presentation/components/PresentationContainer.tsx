af0c3528c8a5fc2445b0919682c25bc25b15ec18
German Hornus <hornusgerman@gmail.com>
2022-05-04 15:30:09 -0300

    add photo

--- src/components/2.Presentation/components/PresentationContainer.tsx
+++ src/components/2.Presentation/components/PresentationContainer.tsx
@@ -4,7 +4,7 @@ export const PresentationContainer = () => {
   return (
     <main className={css.container}>
       <div className={css.front}>
-        <img src="/images/person.png" alt="" />
+        <img src="/images/person.jpg" alt="" />
       </div>
       <div className={`${css.ring} ${css.ringFront}`}></div>
     </main>
