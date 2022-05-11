93bb666b9ee93561917f0c5c6f9d721b4c04b145
German Hornus <hornusgerman@gmail.com>
2022-05-11 04:01:04 -0300

    update skills

--- src/components/4.ContainerCards/ContainerCards.tsx
+++ src/components/4.ContainerCards/ContainerCards.tsx
@@ -1,30 +1,13 @@
-import { Card } from './components/Card'
 import css from './app.module.scss'
+import { Card } from './components/Card'
+import { projectsData } from './data'
 
 export const ContainerCards = () => {
   return (
     <section className={css.containerCards}>
-      <Card
-        title="Space Tourism"
-        siteURL="https://space-tourism-site-charly8888.netlify.app"
-        github="https://github.com/charly8888/space-tourism-website"
-        siteImage="/images/space-tourism.png"
-        description="A site about planets, astronauts and tecnology"
-      />
-      <Card
-        title="Ecommerce"
-        siteURL="https://e-comerce-challenge.netlify.app/"
-        github="https://github.com/charly8888/challenge_e-commerce"
-        siteImage="/images/ecommerce.png"
-        description="E-commerce that provide an user and points for claim products"
-      />
-      <Card
-        title="Robot Color"
-        siteURL="https://robot-color.netlify.app/"
-        github="https://github.com/charly8888/robot-colors"
-        siteImage="/images/robot.png"
-        description="This is my first project, I work in animations and interactions with the DOM"
-      />
+      {projectsData.map((project) => {
+        return <Card {...project} key={project.title} />
+      })}
     </section>
   )
 }
---
+++ src/components/4.ContainerCards/data.ts
@@ -0,0 +1,34 @@
+interface project {
+  title: string
+  siteURL: string
+  github: string
+  siteImage: string
+  description: string
+}
+type data = project[]
+
+export const projectsData: data = [
+  {
+    title: 'Space Tourism',
+    siteURL: 'https://space-tourism-site-charly8888.netlify.app',
+    github: 'https://github.com/charly8888/space-tourism-website',
+    siteImage: '/images/space-tourism.png',
+    description: 'A site about planets, astronauts and tecnology',
+  },
+  {
+    title: 'Ecommerce',
+    siteURL: 'https://e-comerce-challenge.netlify.app/',
+    github: 'https://github.com/charly8888/challenge_e-commerce',
+    siteImage: '/images/ecommerce.png',
+    description:
+      'E-commerce that provide an user and points for claim products',
+  },
+  {
+    title: 'Robot Color',
+    siteURL: 'https://robot-color.netlify.app/',
+    github: 'https://github.com/charly8888/robot-colors',
+    siteImage: '/images/robot.png',
+    description:
+      'This is my first project, I work in animations and interactions with the DOM',
+  },
+]
