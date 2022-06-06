75527a7a145ac887826413a96c8a95c49d8311cf
German Hornus <hornusgerman@gmail.com>
2022-06-06 19:33:28 -0300

    looks good

--- src/components/Navbar/Header.tsx
+++ src/components/Navbar/Header.tsx
@@ -44,19 +44,11 @@ export const Header = () => {
       </header>
       <button onClick={() => setIsOpen(!isOpen)} className={css.ImgMenu}>
         {isOpen ? (
-          <svg
-            height="48"
-            width="48"
-            fill="#fff"
-          >
+          <svg height="48" width="48" fill="#fff">
             <path d="M12.45 37.65 10.35 35.55 21.9 24 10.35 12.45 12.45 10.35 24 21.9 35.55 10.35 37.65 12.45 26.1 24 37.65 35.55 35.55 37.65 24 26.1Z" />
           </svg>
         ) : (
-          <svg
-            height="48"
-            width="48"
-            fill="#fff"
-          >
+          <svg height="48" width="48" fill="#fff">
             <path d="M6 36V33H42V36ZM6 25.5V22.5H42V25.5ZM6 15V12H42V15Z" />
           </svg>
         )}
